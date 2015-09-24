2c3606ca16ab095a3bd0d5356f514fe31e67ec34
Arne Schauf <git@as-webservices.de>
2015-09-24 23:57:03 +0200

    do not automatically flash default config

--- flash_initial.sh
+++ flash_initial.sh
@@ -5,7 +5,7 @@ set -e
 ./luatool.py -f init.lua
 ./luatool.py -f pluginloader.lua
 ./luatool.py -f wificonnect.lua
-./luatool.py -f config.lua
+# ./luatool.py -f config.lua
 # ./luatool.py -f wsledtest.lua
 # ./luatool.py -c -f drf_api.lua
 # ./luatool.py -f dht_reader.lua
